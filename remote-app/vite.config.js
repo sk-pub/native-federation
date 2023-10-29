90f09ace79e9cdfd0567959953709b73d13fad7b
Sergei Koshel <2191338+sk-pub@users.noreply.github.com>
2023-10-29 11:58:10 +0100

    Use remote app in shell via native federation

--- remote-app/vite.config.js
+++ remote-app/vite.config.js
@@ -4,18 +4,24 @@ import { federation } from "@module-federation/vite";
 import { createEsBuildAdapter } from "@softarc/native-federation-esbuild";
 
 export default defineConfig(async ({ command }) => {
-    // npm run dev
+    // npm run dev, npm run preview
     if (command === 'serve') {
         return {
             build: {
                 outDir: "dist",
                 emptyOutDir: true,
                 lib: {
-                entry: resolve(__dirname, "src/main.ts"),
-                name: "RemoteApp",
-                fileName: "main",
-                formats: ["es"],
+                    entry: resolve(__dirname, "src/main.ts"),
+                    name: "RemoteApp",
+                    fileName: "main",
+                    formats: ["es"],
                 },
+            },
+            preview: {
+                port: 4170
+            },
+            server: {
+                port: 4170
             }
         };
     }
--- shell/src/main.ts
+++ shell/src/main.ts
@@ -1,24 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { initFederation, loadRemoteModule } from '@softarc/native-federation';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const config = { "remote-app" : "http://localhost:4170/remoteEntry.json" };
+await initFederation(config);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+await loadRemoteModule({
+  remoteName: 'remote-app',
+  exposedModule: './main',
+});
